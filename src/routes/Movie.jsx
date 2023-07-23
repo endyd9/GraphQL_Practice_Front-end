2e3c295b30ecc8f458622394087191b14f829a0c
endyd9 <endyd9@gmail.com>
2023-07-24 00:02:28 +0900

    Movie WebApp

--- src/routes/Movie.jsx
+++ src/routes/Movie.jsx
@@ -1,25 +1,99 @@
 import { gql, useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
 
 export const GET_MOVIE = gql`
   query getMovie($movieId: String!) {
     movie(id: $movieId) {
       id
       title
+      medium_cover_image
+      rating
+      isLiked @client
     }
   }
 `;
 
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 10px;
+  width: 50%;
+`;
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+  font-size: 28px;
+`;
+
+const Image = styled.div`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
+  background-image: url(${(props) => props.bg});
+  background-size: cover;
+  background-position: center center;
+  border-radius: 7px;
+`;
+
+const Btn = styled.button`
+  background-color: rgba(255, 255, 255, 0);
+  border: none;
+  font-size: 40px;
+  color: red;
+`;
+
 export default function Movie() {
   const { id } = useParams();
-  const { data, loading } = useQuery(GET_MOVIE, {
+  const {
+    data,
+    loading,
+    client: { cache },
+  } = useQuery(GET_MOVIE, {
     variables: {
       movieId: id,
     },
   });
-  console.log(data, loading);
-
-  if (loading) return <h1>loading...</h1>;
-
-  return <h1>{data.movie.title}</h1>;
+  const onClick = () => {
+    cache.writeFragment({
+      id: `Movie:${id}`,
+      fragment: gql`
+        fragment MovieFragment on Movie {
+          isLiked
+        }
+      `,
+      data: {
+        title: "You Hacked",
+        rating: 444,
+        isLiked: !data?.movie.isLiked,
+      },
+    });
+  };
+  return (
+    <Container>
+      <Column>
+        <Title>{loading ? "Loading..." : `${data?.movie?.title}`}</Title>
+        <Subtitle>⭐️ {data?.movie?.rating}</Subtitle>
+        <Btn onClick={onClick}>{data?.movie?.isLiked ? "♥" : "♡"}</Btn>
+      </Column>
+      <Image bg={data?.movie?.medium_cover_image} />
+    </Container>
+  );
 }
